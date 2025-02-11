2f9ef538ba856e56c534300908f006e08f0a4be5
Emre K. <9091762+emre1393@users.noreply.github.com>
2025-02-11 09:55:19 +0300

    updated keenkit.sh url with english/ru one.
    updated keenkit.sh url with english/ru one.

--- install.sh
+++ install.sh
@@ -10,7 +10,9 @@ if ! opkg list-installed | grep -q "^curl"; then
   opkg install curl
 fi
 
-curl -L -s "https://raw.githubusercontent.com/spatiumstas/$REPO/main/$SCRIPT" --output $TMP_DIR/$SCRIPT
+#curl -L -s "https://raw.githubusercontent.com/spatiumstas/$REPO/main/$SCRIPT" --output $TMP_DIR/$SCRIPT
+#it needs to get translated bash script from fork
+curl -L -s "https://raw.githubusercontent.com/emre1393/KeenKit_English/main/keenkit.sh" --output $TMP_DIR/$SCRIPT
 mv "$TMP_DIR/$SCRIPT" "$OPT_DIR/$SCRIPT"
 chmod +x $OPT_DIR/$SCRIPT
 cd $OPT_DIR/bin
